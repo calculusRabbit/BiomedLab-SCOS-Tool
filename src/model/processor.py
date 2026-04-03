a540b82ba57b8bf661c7cddcd9446f3e6ead8776
Yuanyuan Gao <sy1007212@gmail.com>
2026-04-03 09:44:21 -0500

    calculate for the upper left and right windows

--- src/model/processor.py
+++ src/model/processor.py
@@ -19,6 +19,7 @@ def process_all_data(frame: np.ndarray) -> SCOSResult:
         od = compute_od(frame),
         # below K2 image for left upper panel
         k2_images = (
+            # Vu I don't want to repeat the image to the first window anymore, please see my calculattion for each of the images below
             frame, # The very first bar on the left index 0 raw crop always shown
             compute_k2_t1(frame), # index 1
             compute_k2_t2(frame), # index 2
@@ -29,30 +30,93 @@ def process_all_data(frame: np.ndarray) -> SCOSResult:
     )
 
 
-def compute_k2(frame: np.ndarray):
-    return frame.mean()
+def compute_k2(Kf2):
+    # 
+    return mean(Kf2)
 
-def compute_bfi(frame: np.ndarray):
-    return frame.mean() / 255.0 + np.random.uniform(-0.05, 0.05)
+def compute_bfi(Kf2):
+    return 1/mean(Kf2)
 
-def compute_cc(frame: np.ndarray):
-    return frame.std()
+def compute_cc(windowed_mean):
+    return windowed_mean
 
-def compute_od(frame: np.ndarray):
-    return frame.max()
+def compute_od(windowed_mean):
+    OD = (-log10(abs(windowed_mean)./(ones(nTpts,1)*windowed_mean[0])));
+    return OD
 
 # FOR UPPER LEFT PANEL
+def img_windowed = reshapeWindow(img,window_size)
+    # this function will cut the img into small images, each small image size is window_size times window_size
+    img_width, img_height = get_img_size(img) # Vu please code this function to get width and length of img
+    n_width = floor(img_width/window_size) # calculate how many windows in horizontal x direction
+    n_height = floor(img_height/window_size) # calculate how many windows in vertical y direction
+    new_img_width = n_width*window_size;
+    new_img_height = n_height*window_size;
+    img = img[0:new_img_width-1; 0:new_img_height-1] # here crop the image so it can be divided into small windows
+
+    # calculate which pixels belong to which window
+    row_ind, col_ind = np.meshgrid(new_img_width, new_img_height)
+    window_ind_temp = ceil(row_ind./window_size) + (ceil(col_ind./window_size)-1)*n_height;
+    [~, window_ind] = sort(window_ind_temp(:))
+    img = reshape(img,[],1)
+    img = img(window_ind(:),:)
+    img_windowed = reshape(img,window_size*window_size,[])
+    return img_windowed
+
 def compute_k2_t1(frame: np.ndarray):
-    return (frame * 0.8)
+    # This is K_raw_squared
+    # here the frame should be original frame minus average dark
+    window_size = 7
+    frame_windowed = reshapeWindow(frame,window_size)
+    windowed_mean = np.mean(frame_windowed,1)
+    windowed_var = np.var(frame_windowed,1)
+    K_raw_squared = windowed_var./windowed_mean
+    # here the size of K_raw_squared should be 7 times smaller than the frame
+    return (K_raw_squared)
 
 def compute_k2_t2(frame: np.ndarray):
-    return (frame * 0.6)
+    # this is K_s_squared
+    gain = get_gain() # we should know what is the gain of the camera here
+    Ks2 = Gain./(windowed_mean);
+    return (Ks2)
 
 def compute_k2_t3(frame: np.ndarray):
-    return (frame * 0.4)
+    # this is Kr2
+    window_size = 7
+    frame_windowed = reshapeWindow(frame,window_size)
+    windowed_mean = np.mean(frame_windowed,1)
+    dark_windowed = reshapeWindow(average_dark_img,window_size)
+    windowed_variance_dark = np.var(dark_windowed)
+    Kr2 = (mean(windowed_variance_dark) - 1/12)./((windowed_mean.^2));
+    return (Kr2)
 
-def compute_k2_t4(frame: np.ndarray):
-    return (frame * 0.2)
+def compute_k2_t4(frame: np.ndarray, previous_frames):
+    # this is Ksp2
+    # here the previous_frames hold <50 previous frames right before the current frame; I don't want to hold too many frames in the memory
+    if length(previous_frames) < 50: # when we don't have 50 frames yet
+        Ksp2 = zeros(size(frame))
+    else:
+        window_size = 7
+        mean_50_frames = np.mean(previous_frames)
+        mean_50_frames_windowed = reshapeWindow(mean_50_frames,window_size)
+        spatial_variance = np.var(mean_50_frames_windowed,0,1);
+        spatial_mean = np.mean(mean_50_frames_windowed,1);
+        spatial_variance = spatial_variance - Gain*spatial_mean/50;
+        Ksp2 = spatial_variance./(spatial_mean.^2);
+    return (Ksp2)
 
 def compute_k2_t5(frame: np.ndarray):
-    return (frame * 0.1)
+    # this is Kq2
+    window_size = 7
+    frame_windowed = reshapeWindow(frame,window_size)
+    windowed_mean = np.mean(frame_windowed,1)
+    Kq2 = 1/12./((windowed_mean.^2));
+    return (Kq2)
+
+def compute_k2_t6(frame: np.ndarray):
+    # I think we should have 6 windows right?
+    # we need all the previous images to calculate this one
+    Kf2 = Kraw2 - Ks2 - Kr2 - Kq2 - Ksp2
+    return (Kf2)
+
+
